4bc5a5044a90964b5f5c023a4d0ef5f4a3a94465
Роман <pangujailbreake58@gmail.com>
2022-11-02 21:43:03 +0300

    Второй комит

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
         Book bookOne = new Book ("Roman s kamnem", "Volcov",1985);
         Book bookTwo = new Book ("Terminator", "Morozov",1996);
-
         Author authorOne = new Author("Ropp", "Serji");
         bookOne.setYearPublication(2000);
         System.out.println(bookOne.getYearPublication());
